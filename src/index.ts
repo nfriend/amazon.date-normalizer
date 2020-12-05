41868cf054b5b67216236e557533235343097f45
Nathan Friend <nathan@gitlab.com>
2020-12-05 00:23:52 -0500

    fix: parse using moment.utc()

--- src/index.ts
+++ src/index.ts
@@ -18,18 +18,18 @@ export const normalize = (amazonDate: string): Moment => {
     // eventDate will look like "2017-W49-WE"
 
     // remove the -WE suffix and add 5 days
-    return moment(amazonDate.replace(/-WE$/, '')).add(5, 'days');
+    return moment.utc(amazonDate.replace(/-WE$/, '')).add(5, 'days');
   } else if (/^\d{3}X$/.test(amazonDate)) {
     // for decades, eventDate will look like '201X'
 
     // just replace the X with a 0 to signify
     // the start of the decade
-    return moment(amazonDate.replace('X', '0'), 'YYYY');
+    return moment.utc(amazonDate.replace('X', '0'), 'YYYY');
   } else if (/^\d{4}-(WI|SP|SU|FA)$/.test(amazonDate)) {
     // for seasons, eventDate will look like '2018-SP'
 
     // transform the date into an exact date, like "2018-03-21"
-    return moment(
+    return moment.utc(
       amazonDate
         .replace(/WI$/, '12-21')
         .replace(/SP$/, '03-21')
@@ -41,24 +41,24 @@ export const normalize = (amazonDate: string): Moment => {
     // some locales return a "month" value - like "next month" -
     // like "2018-11-XX"
 
-    return moment(amazonDate.replace(/XX$/, '01'), 'YYYY-MM-DD');
+    return moment.utc(amazonDate.replace(/XX$/, '01'), 'YYYY-MM-DD');
   } else if (/^\d{4}-XX-XX$/.test(amazonDate)) {
     // some locales return a "year" value - like "next year" -
     // like "2018-XX-XX"
 
-    return moment(amazonDate.replace(/XX-XX$/, '01-01'), 'YYYY-MM-DD');
+    return moment.utc(amazonDate.replace(/XX-XX$/, '01-01'), 'YYYY-MM-DD');
   } else if (amazonDate === 'PRESENT_REF') {
     // if the user said "now"
 
     // convert the date into the expected format
-    return moment();
+    return moment.utc();
   } else if (/^\d{4}-\d{2}-\d{2}$/.test(amazonDate)) {
     // if the date is a standard YYYY-MM-DD
 
     // force moment to parse it to avoid falling back to
     // native Date parsing, which has timezone issues
-    return moment(amazonDate, 'YYYY-MM-DD');
+    return moment.utc(amazonDate, 'YYYY-MM-DD');
   } else {
-    return moment(amazonDate);
+    return moment.utc(amazonDate);
   }
 };
